96d68530b7448ad04a6ba57dcbd42425ab5c6ea2
JP <jp@jpatrickpark.com>
2016-09-22 21:28:48 -0700

    first commit

---
+++ js/conversation.js
@@ -0,0 +1,113 @@
+/**
+ * Modification Copyright 2016 Jungkyu Park
+ * 
+ * There wasn't any copyright notice in original source code.
+ * Refer to NOTICE in top directory for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * MODIFICATIONS
+ * 1. Removed function callschatUpdateSetup(); and 
+ *    Api.sendRequest( '', null ); from ConversationPanel.init() function 
+ *    definition.
+ * 2. Removed inputKeyDown from publicly accesible methods of 
+ *    ConversationPanel.
+ * 3. Removed function definitions callschatUpdateSetup(), inputKeyDown(),
+      displayMessage(), isUserMessage(), buildMessageDomElements(), 
+      and scrollToChatBottom()
+   4. Removed settings variable from ConversationPanel 
+ */
+
+// The ConversationPanel module is designed to handle
+// all display and behaviors of the conversation column of the app.
+/* eslint no-unused-vars: "off" */
+/* global Api: true, Common: true*/
+
+var ConversationPanel = (function() {
+  // Publicly accessible methods defined
+  return {
+    init: init,
+  };
+
+  // Initialize the module
+  function init() {
+    setupInputBox();
+  }
+
+// Set up the input box to underline text as it is typed
+  // This is done by creating a hidden dummy version of the input box that
+  // is used to determine what the width of the input text should be.
+  // This value is then used to set the new width of the visible input box.
+  function setupInputBox() {
+    var input = document.getElementById('textInput');
+    var dummy = document.getElementById('textInputDummy');
+    var minFontSize = 14;
+    var maxFontSize = 16;
+    var minPadding = 4;
+    var maxPadding = 6;
+
+    // If no dummy input box exists, create one
+    if (dummy === null) {
+      var dummyJson = {
+        'tagName': 'div',
+        'attributes': [{
+          'name': 'id',
+          'value': 'textInputDummy'
+        }]
+      };
+
+      dummy = Common.buildDomElement(dummyJson);
+      document.body.appendChild(dummy);
+    }
+
+    function adjustInput() {
+      if (input.value === '') {
+        // If the input box is empty, remove the underline
+        input.classList.remove('underline');
+        input.setAttribute('style', 'width:' + '100%');
+        input.style.width = '100%';
+      } else {
+        // otherwise, adjust the dummy text to match, and then set the width of
+        // the visible input box to match it (thus extending the underline)
+        input.classList.add('underline');
+        var txtNode = document.createTextNode(input.value);
+        ['font-size', 'font-style', 'font-weight', 'font-family', 'line-height',
+          'text-transform', 'letter-spacing'].forEach(function(index) {
+            dummy.style[index] = window.getComputedStyle(input, null).getPropertyValue(index);
+          });
+        dummy.textContent = txtNode.textContent;
+
+        var padding = 0;
+        var htmlElem = document.getElementsByTagName('html')[0];
+        var currentFontSize = parseInt(window.getComputedStyle(htmlElem, null).getPropertyValue('font-size'), 10);
+        if (currentFontSize) {
+          padding = Math.floor((currentFontSize - minFontSize) / (maxFontSize - minFontSize)
+            * (maxPadding - minPadding) + minPadding);
+        } else {
+          padding = maxPadding;
+        }
+
+        var widthValue = ( dummy.offsetWidth + padding) + 'px';
+        input.setAttribute('style', 'width:' + widthValue);
+        input.style.width = widthValue;
+      }
+    }
+
+    // Any time the input changes, or the window resizes, adjust the size of the input box
+    input.addEventListener('input', adjustInput);
+    window.addEventListener('resize', adjustInput);
+
+    // Trigger the input event once to set up the input box and dummy element
+    Common.fireEvent(input, 'input');
+  }
+}());
